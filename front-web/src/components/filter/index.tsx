3a9fc08ce3f81a300a1999404c036dc7aabb084a
Silvio Limeira <sicemal@gmail.com>
2023-04-07 11:59:55 -0400

    useState Gender and onChangeGender event

--- front-web/src/components/filter/index.tsx
+++ front-web/src/components/filter/index.tsx
@@ -3,12 +3,24 @@ import 'flatpickr/dist/themes/material_green.css';
 import FlatPicker from 'react-flatpickr';
 import { Portuguese } from 'flatpickr/dist/l10n/pt';
 import flatpickrlib from 'flatpickr';
+import React, { useState } from 'react';
+import { Gender } from '../../types';
 
 flatpickrlib.localize(Portuguese);
 
 function Filter() {
+  const [dates, setDates] = useState<Date[]>([]);
+  const [gender, setGender] = useState<Gender>();
+
   const onChangeDate = (dates: Date[]) => {
-    console.log(dates);
+    if (dates.length == 2) {
+      setDates(dates);
+    }
+  };
+
+  const onChangeGender = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedGender = event.target.value as Gender;
+    setGender(selectedGender);
   };
 
   return (
@@ -23,11 +35,11 @@ function Filter() {
         onChange={onChangeDate}
         placeholder="Selecione um período"
       />
-      <select className="filter-input">
+      <select className="filter-input" value={gender} onChange={onChangeGender}>
         <option value="">Selecione um gênero</option>
-        <option value="MALE"></option>
-        <option value="FEMALE"></option>
-        <option value="OTHER"></option>
+        <option value="MALE">Masculino</option>
+        <option value="FEMALE">Feminino</option>
+        <option value="OTHER">Outro</option>
       </select>
     </div>
   );
